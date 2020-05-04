ff5f3fd18881a84abe7b87708907fd58416eeab8
Paolo <paolo.suero@gmail.com>
2020-05-04 20:19:32 +0200

    Esercizio completo (con bonus)

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
 //HO AGGIUNTO QUALCHE PASSAGGIO IN PIU' E MOLTI COMMENTI SPECIFICI PER CAPIRE OGNI AZIONE
 
 //creo l'interazione al "carousel-next"
-//intercetto il click sull'icona next
+//intercetto il click sull'icona "carousel-right" ed utilizzo la funzione "next"
 $(".carousel-right").click(next)
 
-
 //creo l'interazione al "carousel-prev"
-//intercetto il click sull'icona
+//intercetto il click sull'icona carousel-left ed utilizzo la funzione "prev"
 $(".carousel-left").click(prev)
-
 
 //creo l'interazione al click sul singolo pallino
 $(".bullets i").click(function() {
-
     //recupero il pallino cliccato
     var bullets_clicked = $(this);
     //recupero l'indice del pallino cliccato
     var ind_bullets_clicked = bullets_clicked.index();
 
-    //seleziono l'immagine con classe active
-    var immagine_corrente = $("img.active");
-    //rimuovo la classe active all'immagine corrente
-    immagine_corrente.removeClass("active");
+    //seleziono l'immagine con classe active e gli rimuovo la classe active
+    var immagine_corrente = $("img.active").removeClass("active");
 
     //seleziono l'immagine con indice uguale al punto selezionato
     var immagine_parallela = $(".slide img").eq(ind_bullets_clicked);
     //aggiungo la classe active all'immagine parallela
     immagine_parallela.addClass("active");
 
-    //seleziono il pallino con classe active
-    var bullets_corrente = $(".bullets i.active");
-    //elimino la classe active al pallino
-    bullets_corrente.removeClass("active");
+    //seleziono il pallino con classe active e gli rimuovo la classe active
+    var bullets_corrente = $(".bullets i.active").removeClass("active");
+
     //aggiungo la classe active al pallino cliccato
     bullets_clicked.addClass("active");
 })
 
-//aggiungo lo scorrimento automatico 
-setInterval(next, 3000)
+//aggiungo lo scorrimento automatico all'apertura della pagina web
+var autoplay = setInterval(next, 3000);
+
+//avvio l'autoplay al click del pulsante display
+$(".play").click(function() {
+    autoplay = setInterval(next, 3000);
+
+    //seleziono l'icona pause e gli rimuovo la classe active
+    $(".pause").removeClass("active");
+
+    //selezioni l'icona play e gli aggiungo la classe active
+    $(".play").addClass("active");
+})
+
+//interrompo l'intervallo al click dei pulsanti: pausa e carousel (left e right)
+$(".pause, .carousel-left, .carousel-right").click(function() {
+    //interrompo l'intervallo designato con variabile "autoplay"
+    clearInterval(autoplay);
+
+    //seleziono il pulsante play e gli rimuovo la classe active
+    $(".play").removeClass("active");
+
+    //seleziono il pulsante pause e gli assegno la classe active
+    $(".pause").addClass("active");
+})
 
 //generalizzo la funzione next
 function next () {
